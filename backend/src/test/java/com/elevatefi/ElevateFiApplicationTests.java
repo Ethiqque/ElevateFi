4ffcabf0f3cb2d8c9c4974d18d29716520d921e9
Daniel Krasnov <danielpyld.work@gmail.com>
2024-09-14 06:50:08 +0300

    initial commit

---
+++ backend/src/test/java/com/elevatefi/ElevateFiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevatefi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevateFiApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
